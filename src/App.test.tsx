9166601f8051e7cdd697de499b74b61ed45de2f2
Kavelaa <37141924+Kavelaa@users.noreply.github.com>
2019-06-11 21:43:26 +0800

    gh-pages

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import AppLTR from './AppLTR'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<AppLTR />, div)
-  ReactDOM.unmountComponentAtNode(div)
-})
